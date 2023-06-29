b24af5e116fb79b43b465cfedfcd4bd08b28bc7e
IvanB <ivan_bylina@epam.com>
2023-06-29 10:16:56 +0200

    orphan

---
+++ tf-orphan/providers.tf
@@ -0,0 +1,14 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "=3.37.0"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+  # use_oidc            = true
+  storage_use_azuread = true
+}
